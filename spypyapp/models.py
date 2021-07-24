d1d8121f85c94321d09dc3b48d5054f103b8497a
josphat-otieno <josephat.otieno@students.moringaschool.com>
2021-07-25 02:38:37 +0300

    created model neighborhood

--- spypyapp/models.py
+++ spypyapp/models.py
@@ -1,4 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Neighbor(models.Model):
+    name =models.CharField(max_length=60)
+    location = models.CharField(max_length=60)
+    occupants_count = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.name
 
