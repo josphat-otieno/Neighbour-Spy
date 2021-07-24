87a97b6faf7de97eb48633683aea10584aae4fbd
josphat-otieno <josephat.otieno@students.moringaschool.com>
2021-07-24 23:31:07 +0300

    created view for function registration

--- spypyapp/urls.py
+++ spypyapp/urls.py
@@ -3,9 +3,12 @@ from django.conf.urls import url
 from django.conf.urls.static import static
 from . import views
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [ 
     url(r'^$', views.index, name='index'),
+    url('register/',views.register, name='registration'),
+    url('login/', auth_views.LoginView.as_view(), name='login'),
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- spypyapp/views.py
+++ spypyapp/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
-
+from .forms import *
+from django.contrib.auth import authenticate, login
+ 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def index(request):
     return render(request, 'spy/index.html')
 
+def register(request):
+    if request.method=="POST":
+        form=RegistrationForm(request.POST)
+        if form.is_valid():
+                form.save()
+                username = form.cleaned_data.get('username')
+                raw_password = form.cleaned_data.get('password1')
+                user = authenticate(username=username, password=raw_password)
+                login(request, user)
+        return redirect('login')
+    else:
+        form= RegistrationForm()
+    return render(request, 'django_registration/registration_form.html', {"form":form})
