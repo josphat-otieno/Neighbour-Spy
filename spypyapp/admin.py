004cbdea335e40cb7d15f329cecfefd8d368e8e7
josphat-otieno <josephat.otieno@students.moringaschool.com>
2021-07-25 12:25:11 +0300

    registered models

--- spypyapp/admin.py
+++ spypyapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Profile, Neighbor, Business, User
 
 # Register your models here.
+admin.site.register(Neighbor)
+admin.site.register(Profile)
+admin.site.register(Business)
